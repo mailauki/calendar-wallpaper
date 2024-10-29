b5f066f2b1d4ae58a785d766b4f584b3217ba589
Julie Evans <juliemevans17@gmail.com>
2024-10-29 13:04:45 -0500

    Update navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -41,7 +42,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            {/* <Navbar /> */}
+            <Navbar />
             <main className="container mx-auto max-w-7xl p-8 flex-grow">
               {children}
             </main>
