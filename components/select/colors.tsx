6c1dc9acbfc2ac16551bfa560940c69e2e3c80d7
Julie Evans <juliemevans17@gmail.com>
2024-11-04 14:18:35 -0600

    Add color validation

--- components/select/colors.tsx
+++ components/select/colors.tsx
@@ -1,6 +1,8 @@
 // import "@/styles/color-picker.css";
 
 import { Divider, Input } from "@nextui-org/react";
+import { getContrastRatio, getContrastColor, RGB } from "a11y-contrast-color";
+import { hex2rgb, rgb2hex } from "@nextcss/color-tools";
 
 export default function ColorsSelect({
   bgColor,
@@ -13,31 +15,81 @@ export default function ColorsSelect({
   setTextColor: React.Dispatch<React.SetStateAction<string>>;
   textColor: string;
 }) {
+  const color1 = hex2rgb(bgColor) as RGB;
+  const color2 = hex2rgb(textColor) as RGB;
+  const contrastRatio = getContrastRatio(color1, color2);
+  const contrastColor1 = getContrastColor(color1, 4.5);
+  const contrastColor2 = getContrastColor(color2, 4.5);
+  const contrastBgColor = rgb2hex(contrastColor1 as number[]);
+  const contrastTextColor = rgb2hex(contrastColor2 as number[]);
+
   return (
     <>
       <div className="flex items-center justify-between px-2 py-4 gap-2">
         <div className="flex flex-col items-center gap-1 w-1/2">
           <Input
             // className="p-1 h-10 w-14 inline-block bg-background border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 hover:border-gray-300 hover:dark:border-neutral-600"
+            classNames={{
+              label: "text-xs text-default-500",
+              helperWrapper: "p-0",
+              mainWrapper: "gap-none",
+              inputWrapper: "rounded-b-none border-b",
+            }}
+            description={
+              <Input
+                classNames={{
+                  inputWrapper: "rounded-t-none border-t",
+                }}
+                errorMessage={`Try ${contrastBgColor}`}
+                isInvalid={contrastRatio < 4.5}
+                type="text"
+                value={bgColor}
+                variant="bordered"
+                onValueChange={setBgColor}
+              />
+            }
+            isInvalid={contrastRatio < 4.5}
+            label="Background Color"
+            labelPlacement="outside"
             type="color"
             value={bgColor}
             variant="bordered"
             onValueChange={setBgColor}
           />
-          <p className="text-xs text-default-500">Background Color</p>
+          {/* <p className="text-xs text-default-500">Background Color</p> */}
         </div>
 
         <Divider className="h-16" orientation="vertical" />
 
         <div className="flex flex-col items-center gap-1 w-1/2">
           <Input
-            // className="p-1 h-10 w-14 inline-block bg-background border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 hover:border-gray-300 hover:dark:border-neutral-600"
+            classNames={{
+              label: "text-xs text-default-500",
+              helperWrapper: "p-0",
+              mainWrapper: "gap-none",
+              inputWrapper: "rounded-b-none border-b",
+            }}
+            description={
+              <Input
+                classNames={{
+                  inputWrapper: "rounded-t-none border-t",
+                }}
+                errorMessage={`Try ${contrastTextColor}`}
+                isInvalid={contrastRatio < 4.5}
+                type="text"
+                value={textColor}
+                variant="bordered"
+                onValueChange={setTextColor}
+              />
+            }
+            isInvalid={contrastRatio < 4.5}
+            label="Text Color"
+            labelPlacement="outside"
             type="color"
             value={textColor}
             variant="bordered"
             onValueChange={setTextColor}
           />
-          <p className="text-xs text-default-500">Text Color</p>
         </div>
       </div>
     </>
